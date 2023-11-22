7ba2fb4fac33e57a6e1f64c111999ce7e7719b2f
amirulhidayah <hidayahamirul103@gmail.com>
2023-11-22 16:37:05 +0800

    (feat) : Hapus beberapa komentar

--- seeders.py
+++ seeders.py
@@ -4,7 +4,6 @@ from models import Milestones,Pengajar
 from app import connex_app
 from datetime import datetime
 
-# Data to initialize the database with
 DAFTAR_PENGAJAR = [
     {'nama': 'Abdillah Fajar', 'keahlian': 'Back End', 'email': 'abdillahfajar@example.com'},
     {'nama': 'Rizky Akbar', 'keahlian': 'Front End', 'email': 'rizkyakbar@example.com'},
@@ -18,15 +17,12 @@ MILESTONES = [
     {'pelajaran': 'Testing and Debugging', 'pencapaian': 'Write Unit Tests', 'keterangan': 'Develop and execute unit tests for critical components', 'is_complete': False, 'pengajar_id': 3},
 ]
 
-# Delete database file if it exists currently
 if os.path.exists('milestones.db'):
     os.remove('milestones.db')
 
-# Create the database
 with connex_app.app.app_context():
     db.create_all()
 
-    # Seed Pengajars
     for pengajar_data in DAFTAR_PENGAJAR:
         pengajar = Pengajar(
             nama=pengajar_data['nama'],
